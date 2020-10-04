aab94b1d8b3c0fdab825da03657f5ee81898508f
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-10-04 17:15:00 +0530

    server receives messages continuously

--- server.cpp
+++ server.cpp
@@ -1,56 +1,71 @@
-#include <bits/stdc++.h>
-#include <netinet/in.h> // For sockaddr_in
-#include <sys/socket.h> // For socket functions
-#include <unistd.h>
+#include <cstring>      // for C strings
+#include <iostream>     // for standard input/output
+#include <netinet/in.h> // for sockaddr_in
+#include <sys/socket.h> // for socket functions
+#include <unistd.h>     // for read and close
 using namespace std;
 
 int main() {
-  // Create a socket (IPv4, TCP)
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    std::cout << "Failed to create socket. errno: " << errno << std::endl;
+  // create a socket (IPv4, TCP)
+  int server = socket(AF_INET, SOCK_STREAM, 0);
+
+  // in case of failure
+  if (server == -1) {
+    printf("Failed to create socket (error code %d). Exiting...\n", errno);
     exit(EXIT_FAILURE);
   }
 
-  // Listen to port 9999 on any address
-  sockaddr_in sockaddr;
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-  sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
-                                   // network byte order
-  if (bind(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0) {
-    std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
+  cout << "Server socket initialised.\n";
+
+  // sockaddr_in structure is capable of handling internet addresses
+  sockaddr_in address;
+  address.sin_family = AF_INET; // to communicate over TCP/IP, using IPv4
+  address.sin_addr.s_addr = INADDR_ANY; // not using a specific IP
+  address.sin_port = htons(9999); // to convert from host byte to network byte
+
+  // bind socker to address and check if a non zero code is returned
+  if (bind(server, (sockaddr *)&address, sizeof(address)) < 0) {
+    printf("Failed to bind to port 9999 (error code %d). Exiting...\n", errno);
     exit(EXIT_FAILURE);
   }
 
-  // Start listening. Hold at most 10 connections in the queue
-  if (listen(sockfd, 10) < 0) {
-    std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+  cout << "Socket bound to port 9999.\n";
+
+  // start listening, allow at most 1 connection to the socket
+  if (listen(server, 1) < 0) {
+    printf("Failed to listen on socket (error code %d). Exiting...\n", errno);
     exit(EXIT_FAILURE);
   }
 
-  // Grab a connection from the queue
-  auto addrlen = sizeof(sockaddr);
-  int connection =
-      accept(sockfd, (struct sockaddr *)&sockaddr, (socklen_t *)&addrlen);
+  cout << "Listening for connections...\n";
+
+  // grab a connection from the queue
+  size_t addrLen = sizeof(address);
+  int connection = accept(server, (sockaddr *)&address, (socklen_t *)&addrLen);
   if (connection < 0) {
-    std::cout << "Failed to grab connection. errno: " << errno << std::endl;
+    printf("Failed to grab connection (error code %d). Exiting...\n", errno);
     exit(EXIT_FAILURE);
   }
 
-  // Read from the connection
-  char buffer[100];
-  auto bytesRead = read(connection, buffer, 100);
-  std::cout << "The message was: " << buffer;
+  cout << "Connected to client.\n";
 
-  bytesRead = read(connection, buffer, 100);
-  std::cout << "The message was: " << buffer;
+  // two buffers to print messages only when buffer changes
+  char buffer[100], oldBuffer[100];
+
+  // keep listening for messages until "end connection" is received
+  while (true) {
+    read(connection, buffer, 100);      // read 100 bytes from buffer
+    if (strcmp(buffer, oldBuffer) != 0) // if something has changed in buffer
+      printf("New message received from client: %s\n", buffer);
+
+    strcpy(oldBuffer, buffer); // replace old buffer with current buffer
+  }
 
   // Send a message to the connection
-  std::string response = "Good talking to you\n";
-  send(connection, response.c_str(), response.size(), 0);
+  // std::string response = "Good talking to you\n";
+  // send(connection, response.c_str(), response.size(), 0);
 
   // Close the connections
   close(connection);
-  close(sockfd);
+  close(server);
 }
